187ab0072ce20086ca3b40c2322376b6d9799955
mpfourt <diazeldars@gmail.com>
2025-02-27 03:28:30 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 import Header from './components/Header.vue'
 import Slider from './components/Slider.vue'
 import '@/assets/main.css';
+import MovieList from './components/MovieList.vue'
 </script>
 
 <template style="max-width:1280px">
@@ -13,5 +14,8 @@ import '@/assets/main.css';
 
   <main>
     <Slider />
+    <div class="back-greyed">
+    </div>
+    <MovieList/>
   </main>
 </template>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Image from '../assets/images/image-movies.png'
+</script>
+<template>
+<div class="w-1200 top-post relative">
+    <div class="grid grid-cols-5 gap-10">
+        <div>
+            <div class="relative">
+            <span class="absolute pos-right-top">7.0</span>
+            <img :src="Image" alt="Logo" class="fitted"/>
+            </div>
+            <div class="text-white font-normal">Wonder Woman 1984</div>
+            <div>2020</div>
+        </div>
+    </div>
+</div>
+</template>
